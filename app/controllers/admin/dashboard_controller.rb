941bc360b93d36471f82481e2da0f8c2d722de16
ACristoff <acristoffanini@gmail.com>
2021-11-11 04:21:59 +0000

    basic authentication for admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  # app/controllers/application_controller.rb
+  # http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], 
+  # password: ENV["HTTP_BASIC_PASSWORD"]\
+  # p ENV['USER1'], ENV['PASSWORD1']
+  http_basic_authenticate_with :name => ENV['USER1'], :password => ENV['PASSWORD1']
+  # p ENV
+  
   def show
   end
 end
