a32b8a5ee86d8781c7c25a033d9bf6b6c78e8535
ACristoff <acristoffanini@gmail.com>
2021-11-16 00:30:49 +0000

    testing rspec 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211116000933) do
+ActiveRecord::Schema.define(version: 20211112025003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,11 +61,6 @@ ActiveRecord::Schema.define(version: 20211116000933) do
     t.string "password_digest"
     t.string "first_name"
     t.string "last_name"
-  end
-
-  create_table "widgets", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "line_items", "orders"
